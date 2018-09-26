c6ad67daf451396bffc1b56073939638d2aa08d1
Jaroslaw Burzawa <jaroslaw.burzawa@balluff.pl>
2018-09-26 09:36:43 +0200

    Dodaj pliki projektów.

---
+++ Cwiczenie_punkt1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_punkt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punkt nowyPunkt = new Punkt(0, 0);
+
+            Console.WriteLine("Współrzędna X: {0}", nowyPunkt.WSPX);
+            Console.WriteLine("Współrzędna Y: {0}", nowyPunkt.WSPY);
+
+            Punkt nowyPunkt2 = new Punkt(1, 1);
+            Console.WriteLine("Współrzędna X1: {0}", nowyPunkt2.WSPX);
+            Console.WriteLine("Współrzędna Y1: {0}", nowyPunkt2.WSPY);
+            
+            
+
+
+            /*nowyPunkt.PrzesunWGore(5);
+            Console.WriteLine("Współrzędna X: {0}", nowyPunkt.WSPX);
+            Console.WriteLine("Współrzędna Y: {0}", nowyPunkt.WSPY);
+            nowyPunkt.PrzesunWDol(11);
+            Console.WriteLine("Współrzędna X: {0}", nowyPunkt.WSPX);
+            Console.WriteLine("Współrzędna Y: {0}", nowyPunkt.WSPY);
+            nowyPunkt.PrzesunWLewo(8);
+            Console.WriteLine("Współrzędna X: {0}", nowyPunkt.WSPX);
+            Console.WriteLine("Współrzędna Y: {0}", nowyPunkt.WSPY);
+            nowyPunkt.PrzesunWPrawo(12);
+            Console.WriteLine("Współrzędna X: {0}", nowyPunkt.WSPX);
+            Console.WriteLine("Współrzędna Y: {0}", nowyPunkt.WSPY);
+            */
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cwiczenie_punkt1/Punkt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_punkt1
+{
+    class Punkt
+    {
+        int wspX = 0;
+        int wspY = 0;
+        public Punkt(int xWsp, int yWsp)
+        {
+            wspX = xWsp;
+            wspY = yWsp;
+        }
+        public int WSPX { get { return wspX; } }
+        public int WSPY { get { return wspY; } }
+
+        
+        public void PrzesunWPrawo(int przesunX)
+        {
+            wspX = wspX + przesunX;
+        }
+        public void PrzesunWLewo(int przesunX)
+        {
+            wspX = wspX - przesunX;
+        }
+        public void PrzesunWGore(int przesunY)
+        {
+            wspY = wspY + przesunY;
+        }
+        public void PrzesunWDol(int przesunY)
+        {
+            wspY = wspY - przesunY;
+        }
+        /*public static Punkt operator +(Punkt MojPunkt1, Punkt MojPunkt2)
+        {
+            return new Punkt(MojPunkt1.xWsp + MojPunkt2.xWsp); 
+        }*/
+        
+
+    }
+
+}
