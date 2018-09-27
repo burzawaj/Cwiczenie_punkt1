9387505f5cf562ec52105a1140998725d7a8b120
Jaroslaw Burzawa <jaroslaw.burzawa@balluff.pl>
2018-09-27 23:56:21 +0200

    Wprowadzono klase wektor

---
+++ Cwiczenie_punkt1/Wektor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_punkt1
+{
+    class Wektor
+    {
+        //Punkt PoczatekWektora { get; set; }
+        //Punkt KoniecWektora { get; set; }
+        public Wektor(Punkt PoczatekWektora1, Punkt KoniecWektora1 )
+        {
+            //PoczatekWektora = PoczatekWektora1;
+            //KoniecWektora = KoniecWektora1;
+
+        }
+        public static void PoliczWspolrzedneWektora()
+        {
+            return; 
+        }
+    }
+}
